65d4df79decb202926d0938f60153ba80d182dc5
meghana <meghanachinnam12@gmail.com>
2024-07-08 20:56:24 +0530

    done

---
+++ task-2/task-2.js
@@ -0,0 +1,10 @@
+const githubIcon = document.querySelector('.bx bxl-github');
+const linkedinIcon = document.querySelector('.bx bxl-linkedin-square');
+
+
+githubIcon.addEventListener('click', () => {
+    window.location.href = 'https://github.com/Meghana-0304';
+});
+linkedinIcon.addEventListener('click', () => {
+    window.location.href = 'https://www.linkedin.com/in/chinnam-meghana';
+});
