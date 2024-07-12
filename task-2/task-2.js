6e70460da698ea704c88120cc58ddc244ccf2062
meghana <meghanachinnam12@gmail.com>
2024-07-12 13:36:28 +0530

    done

--- task-2/task-2.js
+++ task-2/task-2.js
@@ -1,10 +1,11 @@
-const githubIcon = document.querySelector('.bx bxl-github');
-const linkedinIcon = document.querySelector('.bx bxl-linkedin-square');
+
+// const githubIcon = document.querySelector('.bx bxl-github');
+// const linkedinIcon = document.querySelector('.bx bxl-linkedin-square');
 
 
-githubIcon.addEventListener('click', () => {
-    window.location.href = 'https://github.com/Meghana-0304';
-});
-linkedinIcon.addEventListener('click', () => {
-    window.location.href = 'https://www.linkedin.com/in/chinnam-meghana';
-});
+// githubIcon.addEventListener('click', () => {
+//     window.location.href = 'https://github.com/Meghana-0304';
+// });
+// linkedinIcon.addEventListener('click', () => {
+//     window.location.href = 'https://www.linkedin.com/in/chinnam-meghana';
+// });
